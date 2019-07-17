dd55e0eb36ddc740d1d13426dd2edecbf1f2535a
Shah123Muhammad <shahmuhammad11027@gmail.com>
2019-07-16 23:53:45 -0600

    Add files via upload

---
+++ Digital Clock/DigitalClock.js
@@ -0,0 +1,50 @@
+ 
+ 
+ function showTime(){
+   let date=new Date();
+   let hours=date.getHours();//0-23
+   let minutes=date.getMinutes();//0-59
+   let seconds=date.getSeconds();//0-59
+   
+   
+   let formatHours=convertFormat(hours);
+   
+   hours=checkTime(hours);
+
+   hours=addZero(hours);
+   minutes=addZero(minutes);
+   seconds=addZero(seconds);
+   
+     document.getElementById('clock').innerHTML=`${hours} : ${minutes} : ${seconds} ${formatHours} `   //selecting elements from html.
+}
+ 
+ function convertFormat(time){
+  let format='AM'
+  if(time>=12){
+    format='PM'
+  }	  
+  return format;
+}
+
+function checkTime(time){
+  if(time>12){
+   time=time-12;
+  }
+  if(time===0){
+    time=12;
+  }
+ return time;
+}
+
+
+
+function addZero(time){
+  if(time<10){
+    time="0"+time;
+  }
+  return time;
+}
+
+showTime();
+setInterval(showTime,1000) /*built in function in javascript that takes two arguments the first will be a callback function 
+                    and second argument is time expressed in milliseconds*/
